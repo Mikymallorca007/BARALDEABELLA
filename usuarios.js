793b92e307160d67d143cae47d840539a15f9520
Mikymallorca007 <Mikymallorca@gmail.com>
2025-07-11 19:55:10 +0200

    Update usuarios.js

--- usuarios.js
+++ usuarios.js
@@ -1,6 +1,6 @@
 // usuarios.js
 const usuariosValidos = [
   { usuario: "miahgonzalez", contrasena: "000000" },
-  { usuario: "123", contrasena: "123456" },
+  { usuario: "Miky007", contrasena: "561910" },
   { usuario: "Miky008", contrasena: "561910" }
 ];
