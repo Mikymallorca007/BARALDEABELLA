621b4bb6ed58585bb62d26645b52fe60738bacad
Mikymallorca007 <Mikymallorca@gmail.com>
2025-07-11 19:25:38 +0200

    Add files via upload

---
+++ usuarios.js
@@ -0,0 +1,6 @@
+// usuarios.js
+const usuariosValidos = [
+  { usuario: "juanP", contrasena: "123456" },
+  { usuario: "anaM", contrasena: "654321" },
+  { usuario: "Miky007", contrasena: "561910" }
+];
