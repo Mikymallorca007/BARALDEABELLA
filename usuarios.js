ab0a61eecf213d2b18cab10123ace1115d901164
Mikymallorca007 <Mikymallorca@gmail.com>
2025-07-12 18:51:48 +0200

    Delete usuarios.js

--- usuarios.js
+++
@@ -1,9 +0,0 @@
-// usuarios.js
-const usuariosValidos = [
-  { usuario: "Elisabeth", contrasena: "333333" },  
-  { usuario: "Lidia", contrasena: "281019" },  
-  { usuario: "Miky006", contrasena: "666666" },  
-  { usuario: "Miky008", contrasena: "888888" },
-  { usuario: "Miky007", contrasena: "561910" },
-  { usuario: "miahgonzalez", contrasena: "000000" }
-];
